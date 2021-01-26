c5e3902ed48ea68ba9612c5573df8284c804c61f
Tim <timschngns@aol.com>
2021-01-26 16:19:51 +0100

    lots of changes

--- amasino_dataPrep/init.m
+++ amasino_dataPrep/init.m
@@ -5,8 +5,16 @@ fit_discount_k()
 dataPath=pwd;
 cd(dataPath)
 load('data_source/schneegansEtAl_ET.csv')
+data = schneegansEtAl_ET
+data = data(:, 3
+
+test = data(data(:, 3)>((0.05+0.9*0.2)-0.15) & data(:, 3)<((0.05+0.9*0.2)+0.15), :)
+
+% col 3: x-position (pixels on screen)
+% col 4: y-position (pixels on screen)
+
 cd(strcat(dataPath, '\intermediateCSVs'))
-csvwrite('ET_adj.csv', schneegansEtAl_ET)
+csvwrite('ET_adj.csv', data)
 cd(dataPath)
 
 subjectiveValue()
