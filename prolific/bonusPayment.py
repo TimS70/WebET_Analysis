e26476f310e7b6a4cdf9bc7bf991d82278461331
Tim <timschngns@aol.com>
2021-02-26 13:40:49 +0100

    after interview

---
+++ prolific/bonusPayment.py
@@ -0,0 +1,3 @@
+import os
+
+print("Current Working directory: ", os.getcwd())
---
+++ r_functions/merge_mean_by_subject.R
@@ -0,0 +1,15 @@
+merge_mean_by_subject = function(data, data_source, varName) {
+    grouped = data_source %>%
+        dplyr::group_by(run_id) %>%
+        dplyr::summarise(varName = mean(varName))
+
+    if (varName %in% names(data)) {
+        data = data %>% dplyr::select(!varName)
+    }
+    
+    data = merge(
+        data, 
+        data_source %>% dplyr::select(run_id, varName), 
+        by='run_id')
+    return(data)
+}
