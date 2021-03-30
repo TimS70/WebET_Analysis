12dec656e51073a6cf712d5416aa9329dc64892e
Tim <timschngns@aol.com>
2021-03-30 08:36:29 +0200

    Remove position index and remove inverted y axis

--- data_prep/add_variables/main.py
+++ data_prep/add_variables/main.py
@@ -47,13 +47,9 @@ def add_variables_global(path_origin, path_target):
           f"""n={n_approved_unique} unique participants """
           f"""({n_approved} approved IDs, incl. duplicates). \n""")
 
-    # data_et = invert_y_axis(data_et)
-    # data_trial = invert_y_pos(data_trial)
-
     data_trial = merge_count_by_index(data_trial, data_et, 'x')
 
     # Bug
-    # data_trial = add_position_index(data_trial)
     data_trial = add_window_size(data_trial)
     data_trial = add_exact_trial_duration(data_trial)
     data_trial = add_new_task_nr(data_trial)
